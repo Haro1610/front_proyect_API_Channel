8823b2d27325513383062bf7933c9c1ff128dd61
Haro1610 <haaronro99@live.com-mx>
2022-03-15 21:05:18 -0600

    Se terminan los utltimo cambios de la clase 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front_proyect_API_Channel';
+  //persona = nomre{'pepe'}
+  flag:boolean = true;
+
+  movies: string[] = [
+    'Los tres Huastecos',
+    'Alien',
+    'Batman',
+    'Una pelicula de huevos',
+    'Pineapple express'
+
+  ];
+  
+  name = 'Aaron'
+  constructor(){
+    setTimeout(() =>{
+      this.name = 'Jose';
+      this.movies.push('Los 3 García');
+    },2000);
+  }
+
+  doOnclick(){
+    console.log('Me pushearon!');
+    this.flag = !this.flag
+  }
 }
